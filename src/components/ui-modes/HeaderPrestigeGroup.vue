feb8f02a2f1bb03c7fcd35f495e4697c4ccf3278
FraSharp <f.duca00@gmail.com>
2025-05-23 02:24:39 +0200

    antimatter_dimensions: stylesheet fixes
    Signed-off-by: FraSharp <f.duca00@gmail.com>

--- src/components/ui-modes/HeaderPrestigeGroup.vue
+++ src/components/ui-modes/HeaderPrestigeGroup.vue
@@ -15,9 +15,9 @@ export default {
 
 <template>
   <div class="c-prestige-info-blocks">
-    <HeaderEternityContainer class="l-game-header__eternity" />
-    <HeaderCenterContainer class="l-game-header__center" />
-    <HeaderInfinityContainer class="l-game-header__infinity" />
+    <HeaderEternityContainer class="l-game-header__eternity header-block" />
+    <HeaderCenterContainer class="l-game-header__center header-block" />
+    <HeaderInfinityContainer class="l-game-header__infinity header-block" />
   </div>
 </template>
 
@@ -25,26 +25,34 @@ export default {
 .c-prestige-info-blocks {
   display: flex;
   flex-direction: row;
-  height: 14rem;
+  flex-wrap: wrap; /* Allow items to wrap */
+  justify-content: space-around; /* Distribute space */
+  min-height: 5rem; /* Adjust height as needed, or remove for auto height */
   width: 100%;
   color: var(--color-text);
+  padding: 0.5rem 0; /* Add some vertical padding */
 }
 
+.header-block { /* Common class for all header blocks */
+  flex-grow: 1;
+  flex-basis: auto; /* Allow shrinking and growing */
+  min-width: 150px; /* Minimum width before wrapping/shrinking */
+  margin: 0.25rem; /* Add some margin between blocks */
+  text-align: center; /* Center text within blocks */
+}
+
+/* Remove absolute positioning and fixed widths */
 .l-game-header__eternity {
-  position: absolute;
-  left: calc(25% - 22rem);
-  width: 22rem;
+  /* width: 22rem; */ /* Replaced by flex properties */
 }
 
 .l-game-header__center {
-  position: absolute;
-  right: calc(50% - 25rem);
-  width: 50rem;
+  /* width: 50rem; */ /* Replaced by flex properties */
+  /* Order this in the middle if visual order is important and differs from DOM order */
+  /* order: 0; */ /* Example */
 }
 
 .l-game-header__infinity {
-  position: absolute;
-  right: calc(25% - 22rem);
-  width: 22rem;
+  /* width: 22rem; */ /* Replaced by flex properties */
 }
 </style>
